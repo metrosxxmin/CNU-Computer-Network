21fdb3e2197cd1fcd79e6ba11489a1004747fea6
metrosxxmin <sxxmin.lee@gmail.com>
2019-10-07 19:14:28 +0900

    Add a new interface named BaseLayer.

---
+++ ARP/src/BaseLayer.java
@@ -0,0 +1,44 @@
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+interface BaseLayer {
+    public final int m_nUpperLayerCount = 0;
+    public final String m_pLayerName = null;
+    public final BaseLayer mp_UnderLayer = null;
+    public final ArrayList<BaseLayer> mp_aUpperLayer = new ArrayList<BaseLayer>();
+
+    public String GetLayerName();
+
+    public BaseLayer GetUnderLayer();
+
+    public BaseLayer GetUpperLayer(int nindex); // 0�� 1��
+
+    public void SetUnderLayer(BaseLayer pUnderLayer);
+
+    public void SetUpperLayer(BaseLayer pUpperLayer);
+
+    public default void SetUnderUpperLayer(BaseLayer pUULayer) {
+    }
+
+    public void SetUpperUnderLayer(BaseLayer pUULayer);
+
+    public default boolean Send(byte[] input, int length) {
+        return false;
+    }
+
+    public default boolean Send(String filename) {
+        return false;
+    }
+
+    public default boolean Receive(byte[] input) {
+        return false;
+    }
+
+    public default boolean Receive() {
+        return false;
+    }
+
+    public void set_progressBar(int length, int i);
+
+}
