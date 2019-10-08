725d91dafaf0f4a27eaddf8868b81b5898340141
metrosxxmin <sxxmin.lee@gmail.com>
2019-10-08 23:40:06 +0900

    ARPLayer 설계를 위한 프레임 작성.

--- ARP/src/ARPLayer.java
+++ ARP/src/ARPLayer.java
@@ -1,4 +1,57 @@
+import java.util.ArrayList;
 
-public class ARPLayer {
+public class ARPLayer implements BaseLayer {
+	
+	public int nUpperLayerCount = 0;
+	public String pLayerName = null;
+	public BaseLayer p_UnderLayer = null;
+	public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
+	
+	
+	
+	
+	public boolean Send(byte[] input, int length) {
+		
 
+		return false;
+	}
+
+	
+
+	public boolean Receive(byte[] input) {
+		
+		return true;
+	}
+	
+	@Override
+	public void SetUnderLayer(BaseLayer pUnderLayer) {
+	
+	}
+
+	@Override
+	public void SetUpperLayer(BaseLayer pUpperLayer) {
+		
+	}
+
+	@Override
+	public String GetLayerName() {
+		// TODO Auto-generated method stub
+		return pLayerName;
+	}
+
+	@Override
+	public BaseLayer GetUnderLayer() {
+		return  null;
+	}
+
+	@Override
+	public BaseLayer GetUpperLayer(int nindex) {
+		return null;
+	}
+
+	@Override
+	public void SetUpperUnderLayer(BaseLayer pUULayer) {
+		
+
+	}
 }
